f4b4faa27e8bfebf6e8b9aac02e608d67b6d279c
Scott Fenton <sctfen@gmail.com>
2015-08-27 13:57:36 -0400

    try this with the cassandra package again...

--- cassandra.pp
+++ cassandra.pp
@@ -1,13 +1,27 @@
-yumrepo { 'datastax':
-  name => 'datastax',
-  ensure => 'present',
-  baseurl => 'http://rpm.datastax.com/community',
-  descr => 'DataStax Repo for Apache Cassandra',
-  enabled => 1,
-  gpgcheck => 0
+# yumrepo { 'datastax':
+#   name => 'datastax',
+#   ensure => 'present',
+#   baseurl => 'http://rpm.datastax.com/community',
+#   descr => 'DataStax Repo for Apache Cassandra',
+#   enabled => 1,
+#   gpgcheck => 0
+# }
+
+class { 'cassandra::datastax_repo':
+  before => Class['cassandra']
 }
 
-package { "java-1.8.0-openjdk":
+package { "java-1.7.0-openjdk":
   ensure => "installed",
-  allow_virtual => false
+  allow_virtual => false,
+  before => Class['cassandra']
+}
+
+
+class { 'cassandra':
+  cluster_name    => 'MyCassandraCluster',
+  package_name    => 'dsc20',
+  endpoint_snitch => 'GossipingPropertyFileSnitch',
+  listen_address  => "${::ipaddress}",
+  seeds           => '172.28.128.7'
 }
