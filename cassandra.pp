20a8ddd87b3e5351a40c56c6decff67d3d237821
Scott Fenton <sctfen@gmail.com>
2015-08-27 16:18:24 -0400

    add ordering and some commented out attempts at configuration

--- cassandra.pp
+++ cassandra.pp
@@ -1,27 +1,42 @@
-# yumrepo { 'datastax':
-#   name => 'datastax',
-#   ensure => 'present',
-#   baseurl => 'http://rpm.datastax.com/community',
-#   descr => 'DataStax Repo for Apache Cassandra',
-#   enabled => 1,
-#   gpgcheck => 0
-# }
-
-class { 'cassandra::datastax_repo':
-  before => Class['cassandra']
+yumrepo { 'datastax':
+  name => 'datastax',
+  ensure => 'present',
+  baseurl => 'http://rpm.datastax.com/community',
+  descr => 'DataStax Repo for Apache Cassandra',
+  enabled => 1,
+  gpgcheck => 0,
+  before => Package['dsc20']
 }
+
 
 package { "java-1.7.0-openjdk":
   ensure => "installed",
-  allow_virtual => false,
-  before => Class['cassandra']
+  allow_virtual => false
+}
+
+package { "dsc20":
+  ensure => "installed",
+  allow_virtual => false
 }
 
 
-class { 'cassandra':
-  cluster_name    => 'MyCassandraCluster',
-  package_name    => 'dsc20',
-  endpoint_snitch => 'GossipingPropertyFileSnitch',
-  listen_address  => "${::ipaddress}",
-  seeds           => '172.28.128.7'
-}
+# yaml_setting { "cassandra_yaml_1":
+#   target => '/etc/cassandra/conf/cassandra.yaml',
+#   key => 'seed_providers/parameters/seeds',
+#   value => '172.28.128.3',
+#   require => File['/etc/cassandra/conf/cassandra.yaml']
+# }
+
+# yaml_setting { "cassandra_yaml_2":
+#   target => '/etc/cassandra/conf/cassandra.yaml',
+#   key => 'listen_address',
+#   value => '',
+#   require => File['/etc/cassandra/conf/cassandra.yaml']
+# }
+
+# yaml_setting { "cassandra_yaml_3":
+#   target => '/etc/cassandra/conf/cassandra.yaml',
+#   key => 'cluster_name',
+#   value => 'Desilu',
+#   require => File['/etc/cassandra/conf/cassandra.yaml']
+# }
