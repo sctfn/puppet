6f44028635d4dffe97d4a6e7e4003092a08bb1d4
Scott Fenton <sctfen@gmail.com>
2015-08-28 09:16:45 -0400

    removed YAML declarations

--- cassandra.pp
+++ cassandra.pp
@@ -33,24 +33,3 @@ service { 'cassandra':
   ensure => 'running',
   require => [ File['cassandra_yaml'], Package['java-1.7.0-openjdk'] ]
 }
-
-# yaml_setting { "cassandra_yaml_1":
-#   target => '/etc/cassandra/conf/cassandra.yaml',
-#   key => 'seed_providers/parameters/seeds',
-#   value => '172.28.128.3',
-#   require => File['/etc/cassandra/conf/cassandra.yaml']
-# }
-
-# yaml_setting { "cassandra_yaml_2":
-#   target => '/etc/cassandra/conf/cassandra.yaml',
-#   key => 'listen_address',
-#   value => '',
-#   require => File['/etc/cassandra/conf/cassandra.yaml']
-# }
-
-# yaml_setting { "cassandra_yaml_3":
-#   target => '/etc/cassandra/conf/cassandra.yaml',
-#   key => 'cluster_name',
-#   value => 'Desilu',
-#   require => File['/etc/cassandra/conf/cassandra.yaml']
-# }
