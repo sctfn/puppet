72553505229ded3f452a82d73e8d9dedb965fbee
Scott Fenton <sctfen@gmail.com>
2015-08-31 10:23:50 -0400

    created

---
+++ noaa-cassandra/manifests/repos.pp
@@ -0,0 +1,10 @@
+class { 'cassandra::repos':
+  yumrepo { 'datastax':
+    name => 'datastax',
+    ensure => 'present',
+    baseurl => 'http://rpm.datastax.com/community',
+    descr => 'DataStax Repo for Apache Cassandra',
+    enabled => 1,
+    gpgcheck => 0
+  }
+}
