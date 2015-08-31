0268cabbab56010761d3fad309cb4c6ebf52296f
Scott Fenton <sctfen@gmail.com>
2015-08-31 11:26:15 -0400

    Syntax whoops

--- noaa-cassandra/manifests/init.pp
+++ noaa-cassandra/manifests/init.pp
@@ -39,10 +39,10 @@ class cassandra (
   $seeds = "",
   $listen_address = "",
   $rpc_address = "",
-  $config_file = "/etc/cassandra/conf/cassandra.yaml"
+  $config_file = "/etc/cassandra/conf/cassandra.yaml",
   $package_name = $cassandra::params::package_name,
   $java_version = $cassandra::params::java_version,
-  $config_template = $cassandra::params::config_template,
+  $config_template = $cassandra::params::config_template
 ) inherits cassandra::params {
 
   class { 'cassandra::repos' :
