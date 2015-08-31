df3d98f944047caeaa0f1579ba9fdfb093275a05
Scott Fenton <sctfen@gmail.com>
2015-08-31 10:06:54 -0400

    Fix syntax errors

--- noaa-cassandra/manifests/init.pp
+++ noaa-cassandra/manifests/init.pp
@@ -34,15 +34,15 @@
 #
 # Scott Fenton <sctfen@gmail.com>
 #
-class cassandra {
+class cassandra (
   $cluster_name = "cluster",
   $seeds = "",
   $listen_address = "",
-  $rpc_address = $listen_address,
+  $rpc_address = "",
   $package_name = "dsc20",
-  $java_version => "java-1.7.0-openjdk",
+  $java_version = "java-1.7.0-openjdk",
   $config_template = "/vagrant/puppet/noaa-cassandra/templates/cassandra.yaml.erb",
-  $config_file = "/etc/cassandra/conf/cassandra.yaml",
+  $config_file = "/etc/cassandra/conf/cassandra.yaml" ) {
 
   yumrepo { 'datastax':
     name => 'datastax',
