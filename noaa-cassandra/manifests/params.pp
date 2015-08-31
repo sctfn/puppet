c52d3261b09c20987a0d8d986e502778d86f3533
Scott Fenton <sctfen@gmail.com>
2015-08-31 11:00:35 -0400

    created params.pp

---
+++ noaa-cassandra/manifests/params.pp
@@ -0,0 +1,5 @@
+class cassandra::params {
+  $package_name = "dsc20",
+  $java_version = "java-1.7.0-openjdk",
+  $config_template = "/vagrant/puppet/noaa-cassandra/templates/cassandra.yaml.erb",
+}
